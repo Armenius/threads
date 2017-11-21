9d067b9fb56c091a4022403cbd7dd28eca52aa9d
parmenion <caturyanarmen@gmail.com>
2017-11-21 13:23:54 +0200

    Backend started.

---
+++ php/login.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: parmenion
+ * Date: 21.11.17
+ * Time: 13:21
+ */
+
+    $_POST['email'];
+    $_POST['password'];
---
+++ php/registrate.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: parmenion
+ * Date: 21.11.17
+ * Time: 13:18
+ */
+    $_POST['email'];
+    $_POST['name'];
+    $_POST['password'];
+
+    
